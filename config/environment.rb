a595aee10b104bada8099aaf75e95d6bf08dc0d7
churchwerks <steven.lennis@icloud.com>
2018-01-10 15:09:30 -0600

    db?

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require 'active_record'
 require 'yaml/store'
 require 'ostruct'
 require 'date'
+require 'pry'
 
 
 require 'bundler/setup'
@@ -11,5 +12,8 @@ Bundler.require
 
 # put the code to connect to the database here
 
-
-require_relative "../artist.rb"
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
+require_relative "../lib/artist.rb"
